663e8971f82c216323e78736f74a5a7cfd06e161
Mehmet Emin Meşe <memin1598@gmail.com>
2024-07-28 08:28:32 +0300

    Main components (header, footer, homepage component) is ready

--- bookstore-frontend/tailwind.config.js
+++ bookstore-frontend/tailwind.config.js
@@ -2,9 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    fontFamily: {
+      consumer: ["hinted-subset-AvenirNextLTPro-Regular"],
+    },
     extend: {},
   },
   plugins: [],
-  purge: ["./src/**/*.html", "./src/**/*.js"],
-  mode: "jit",
+  // purge: ["./src/**/*.html", "./src/**/*.js"],
+  // mode: "jit",
 };
